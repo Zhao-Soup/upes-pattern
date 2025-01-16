316be2a45de31e4cbf0ec9289d607717078be217
Zhao-Soup <Yashmehsi@gmail.com>
2025-01-16 13:30:03 +0530

    new commit

---
+++ OneDrive/Documents/sem 6/Pattern recognition/lab/labCodes/lab 1/lab1.py
@@ -0,0 +1,23 @@
+"""
+write examples for the methods and use the functions inside them in an example
+----numpy and pandas----
+>>>numpy
+array creation (1-D,2-D,3-D)
+dimension, size check
+array manipulation
+arithmatic operation
+indexing
+slicing, masking
+shape manipulation
+
+>>>pandas
+import/export data
+cleanup
+selection/indexing
+grouping
+sort/filter
+merge/join
+statistical operation
+
+"""
+
